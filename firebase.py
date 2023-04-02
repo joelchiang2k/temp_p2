aaef02854d2b1abe2e564f3481b068ceafc7adf6
ach1ntya <apinnint@purdue.edu>
2023-04-02 11:37:32 -0400

    add 3 endpoints
    finished create package, delete package by name, registry reset endpoints

---
+++ firebase.py
@@ -0,0 +1,8 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://registrytest-acbf4-default-rtdb.firebaseio.com/'
+})
