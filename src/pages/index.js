36f1ef8cae73859373c05035838ab76f487cedd3
joel <joelchiang2k@gmail.com>
2023-04-07 18:58:43 -0400

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,16 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export async function getStaticProps(context){
-  const res = await fetch('http://localhost:9876/package')
+  const res = await fetch('http://localhost:9876/package',{
+    method: "GET",
+    headers: {
+          // update with your user-agent
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+          Accept: "application/json; charset=UTF-8",
+        },
+  })
   const message = await res.json();
-
   return { 
     props: {message}
     }  ;
