4319730094572613050f2e9f3b892d71e0e31aef
joel <joelchiang2k@gmail.com>
2023-04-11 20:46:07 -0400

    process

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export async function getStaticProps(context) {
   try {
+    console.log("process: ", process.env.BACKEND_API)
     const res = await fetch(`http://${process.env.BACKEND_API}/package`);
     const message = await res.json();
-    console.log(message)
+
     return {
       props: { message }
     };
