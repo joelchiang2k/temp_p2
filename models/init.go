f813b080459d0e1d469262de59d0731847aedebc
brow1770 <brow1770@purdue.edu>
2023-04-16 17:12:09 -0400

    added db update

--- models/init.go
+++ models/init.go
@@ -9,9 +9,9 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 
-	dbURL := "postgres://postgres@localhost:5432/ben_temp"
-
-	database, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	//dbURL := "postgres://postgres@localhost:5432/ben_temp"
+	dsn := "host=146.148.72.91 user=postgres password=461dbpassword dbname=postgres port=5432 sslmode=disable"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
