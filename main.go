24acb437988ddb1fae89d9e0d2785c8d612a901e
Daniel Kholodenko <dkholode@purdue.edu>
2023-05-02 15:11:17 -0400

    local testing

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func main() {
 		api.GET("/:{id}", RetreivePackage)
 		//api.PUT("/:{id}", ADD FUNC FOR PUT)
 		api.DELETE("/:{id}", DeletePackageById)
-		//api.GET("/:{id}/rate, RatePackage")
+		api.GET("/:{id}/rate, RatePackage")
 		api.POST("/byRegEx", controllers.ByRegex)
 	}
 
